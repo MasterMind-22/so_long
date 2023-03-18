0511c4d72d6cf90b06165a71192969e12c9a3e13
home <you.nad3105@gmail.com>
2023-03-17 22:02:12 -0400

    Fri Mar 17 10:02:12 PM EDT 2023

--- so_long.c
+++ so_long.c
@@ -8,17 +8,12 @@ static void	free_fun1(so_long *s_long)
 	free(s_long->map);
 	free(s_long->backtracking_map);
 }
-void leaks(void)
-{
-	system("leaks so_long");
-}
 
 int	main(int ac, char **av)
 {
 	so_long *s_long;
 
 	s_long = malloc(sizeof(so_long));
-	// atexit(leaks);
 	if (ac == 2)
 	{
 		if (ft_strcmp(".ber", av[1]+(ft_strlen(av[1])-4)) != 0)
@@ -30,16 +25,16 @@ int	main(int ac, char **av)
 		read_map(s_long, av[1]);
 		s_long->backtracking_map = read_map1(s_long, av[1]);
 		check_map(s_long, av[1]);
-		// s_long->mlx = mlx_init();
-		// s_long->win = mlx_new_window(s_long->mlx, 60*(s_long->map_width), 60*(s_long->map_height), "so_long");
-		// xpm_to_img(s_long);
-		// print_on_screen(s_long);
-		// mlx_hook(s_long->win, 2, 1L<<0, get_keycode, s_long);
-		// mlx_hook(s_long->win, 17, 1L<<0, close_mark, s_long);
-		// mlx_loop(s_long->mlx);
-		// free_ptr(s_long->map, s_long->map_height);
-		// mlx_destroy_window(s_long->mlx, s_long->win);
-		// free_fun1(s_long);
+		s_long->mlx = mlx_init();
+		s_long->win = mlx_new_window(s_long->mlx, 60*(s_long->map_width), 60*(s_long->map_height), "so_long");
+		xpm_to_img(s_long);
+		print_on_screen(s_long);
+		mlx_hook(s_long->win, 2, 1L<<0, get_keycode, s_long);
+		mlx_hook(s_long->win, 17, 1L<<0, close_mark, s_long);
+		mlx_loop(s_long->mlx);
+		free_ptr(s_long->map, s_long->map_height);
+		mlx_destroy_window(s_long->mlx, s_long->win);
+		free_fun1(s_long);
 	}
 	else
 		ft_putstr("Error\nPlease input a valid argument");
